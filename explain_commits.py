2a7b2b921c21a6dc1372c8543a996744b2a2c4b5
endolith <endolith@gmail.com>
2023-08-02 12:36:26 -0400

    Change to a command line tool

--- explain_commits.py
+++ explain_commits.py
@@ -1,7 +1,9 @@
+import argparse
 import git
+import os
 
 
-def latest_commit_to_file(repo_path, file_name):
+def latest_commit_to_file(repo_path):
     repo = git.Repo(repo_path)
 
     # Get the latest commit
@@ -10,21 +12,27 @@ def latest_commit_to_file(repo_path, file_name):
     # Get the commit message
     commit_message = commit.message
 
-    # Get the diff, excluding certain file types
-    diff = commit.diff('HEAD~1',
-                       create_patch=True,
-                       ignore_blank_lines=True)
+    # Get the diff
+    diff = commit.diff('HEAD~1', create_patch=True)
 
-    diff_text = ""
-    for d in diff:
-        if not d.a_path.endswith(('.hex', '.uvopt')) and not d.diff_binary:
-            diff_text += d.diff + "\n"
+    diff_text = "".join(d.__str__() for d in diff.iter_change_type('M'))
+
+    # Define output file name
+    output_file = os.path.join(repo_path, f"{commit.hexsha}.txt")
 
     # Write to file
-    with open(file_name, 'w') as f:
+    with open(output_file, 'w', encoding='utf-8') as f:
         f.write("Commit Message:\n" + commit_message + "\n")
         f.write("Diff:\n" + diff_text)
 
+    print(f"Commit information written to {output_file}")
 
-# Use the function to write the latest commit to a file
-latest_commit_to_file('/path/to/your/repo', 'commit.txt')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Write latest commit info to a file.")
+    parser.add_argument("path", help="Path to the git repository.")
+
+    args = parser.parse_args()
+
+    latest_commit_to_file(args.path)
