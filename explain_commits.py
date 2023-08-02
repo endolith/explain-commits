f543d12eda5fcd0773283011f409aa66fe47ccf9
endolith <endolith@gmail.com>
2023-08-02 13:25:33 -0400

    Try to add filetype whitelist

--- explain_commits.py
+++ explain_commits.py
@@ -3,7 +3,7 @@ import git
 import os
 
 
-def commit_to_file(repo_path, commit_hash=None):
+def commit_to_file(repo_path, commit_hash=None, include_extensions=None):
     repo = git.Repo(repo_path)
 
     # Get the specified commit, or default to the latest commit
@@ -19,7 +19,8 @@ def commit_to_file(repo_path, commit_hash=None):
     diff_text = ""
     if diff:
         for d in diff.iter_change_type('M'):
-            diff_text += d.__str__()
+            if include_extensions is None or d.a_path.endswith(include_extensions):
+                diff_text += d.__str__()
 
     # Define output file name
     output_file = os.path.join(repo_path, f"{commit.hexsha}.txt")
@@ -40,7 +41,12 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--commit",
                         help="Specific commit hash. If not specified, "
                         "defaults to the latest commit.")
+    parser.add_argument("-i", "--include",
+                        help="Comma-separated list of file extensions to "
+                        "include. Defaults to .c,.h",
+                        default=".c,.h")
 
     args = parser.parse_args()
+    include_extensions = tuple(args.include.split(','))
 
-    commit_to_file(args.path, args.commit)
+    commit_to_file(args.path, args.commit, include_extensions)
