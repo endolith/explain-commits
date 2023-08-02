55af626078a5b7bc0cceb3753ea9bc71013f2add
endolith <endolith@gmail.com>
2023-08-02 13:22:01 -0400

    Fixed the backwards diff order
    and reduce the use of unreadable one-liners

--- explain_commits.py
+++ explain_commits.py
@@ -14,9 +14,12 @@ def commit_to_file(repo_path, commit_hash=None):
 
     # Get the diff
     parent = commit.parents[0] if commit.parents else None
-    diff = commit.diff(parent, create_patch=True)
+    diff = parent.diff(commit, create_patch=True) if parent else None
 
-    diff_text = "".join(d.__str__() for d in diff.iter_change_type('M'))
+    diff_text = ""
+    if diff:
+        for d in diff.iter_change_type('M'):
+            diff_text += d.__str__()
 
     # Define output file name
     output_file = os.path.join(repo_path, f"{commit.hexsha}.txt")
@@ -24,7 +27,8 @@ def commit_to_file(repo_path, commit_hash=None):
     # Write to file
     with open(output_file, 'w', encoding='utf-8') as f:
         f.write("Commit Message:\n" + commit_message + "\n")
-        f.write("Diff:\n" + diff_text)
+        if diff_text:
+            f.write("Diff:\n" + diff_text)
 
     print(f"Commit information written to {output_file}")
 
