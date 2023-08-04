8a8c86879224a4775ec5ef7350e3af6ad0b7151f
endolith <endolith@gmail.com>
2023-08-04 11:03:46 -0400

    No need for commit_message variable
    just use commit.message

--- explain_commits.py
+++ explain_commits.py
@@ -9,9 +9,6 @@ def commit_to_file(repo_path, commit_hash=None, include_extensions=None):
     # Get the specified commit, or default to the latest commit
     commit = repo.commit(commit_hash) if commit_hash else repo.head.commit
 
-    # Get the commit message
-    commit_message = commit.message
-
     # Get the diff
     parent = commit.parents[0] if commit.parents else None
     diff = parent.diff(commit, create_patch=True) if parent else None
@@ -27,7 +24,7 @@ def commit_to_file(repo_path, commit_hash=None, include_extensions=None):
 
     # Write to file
     with open(output_file, 'w', encoding='utf-8') as f:
-        f.write("Commit Message:\n" + commit_message + "\n")
+        f.write("Commit Message:\n" + commit.message + "\n")
         if diff_text:
             f.write("Diff:\n" + diff_text)
 
